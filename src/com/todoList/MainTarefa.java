03c7f8dcad161dbd6e5db4d5e88f1deb1247a9df
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-11 19:36:23 -0300

    iniciando o fluxo de perguntas do programa.

--- src/com/todoList/MainTarefa.java
+++ src/com/todoList/MainTarefa.java
@@ -1,14 +1,27 @@
 package com.todoList;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainTarefa {
 
   public static void main(String[] args) throws IOException {
     GerenciadorDeTarefas minhaTarefa = new GerenciadorDeTarefas();
+    int opcao = 0;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("=== Bem vindo ao sistema de lista de tarefas ===");
+    while (opcao != 3) {
+      System.out.println("Esolha a opção desejada: ");
+      System.out
+          .println("1- Cadastrar nova tarefa\n2- Listar todas as tarefas\3- Sair do programa");
+
+    }
 
     minhaTarefa.novaTarefa("Verificar se salvou no arquivo");
     minhaTarefa.novaTarefa("Verificar se salvou no arquivo2");
+
+    minhaTarefa.listarTarefas();
   }
 
 }
